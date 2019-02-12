8dcda1be9b10d99bc3caeb502392a0441629010e
unknown <mahertag@gmail.com>
2019-02-12 23:01:28 +0200

    updates

--- app/src/main/java/com/example/findcolors/activity/TestActivity.java
+++ app/src/main/java/com/example/findcolors/activity/TestActivity.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
@@ -27,6 +31,7 @@ import java.util.List;
     */
 public class TestActivity extends AppCompatActivity implements PhotoAdapter.PhotoClickListener {
     private List<PhotosModel> photosModels;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +45,7 @@ public class TestActivity extends AppCompatActivity implements PhotoAdapter.Phot
         final PhotoAdapter photoAdapter = new PhotoAdapter(this);
 
         // تحديد اتجاه افقي لعرض الداتا واضافه الادابتر ل recylcer view
-        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recyclerView.setAdapter(photoAdapter);
 
         // عمل وعرض dialog لتنبيه المستخدم بوجود تحميل لمحتوي
@@ -76,25 +81,64 @@ public class TestActivity extends AppCompatActivity implements PhotoAdapter.Phot
     // استقبال ال action الخاص بزرار الرجوع الخاص بالاكتفتي
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             finish();
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void onClick(int pos) {
+    public void onClick(final int pos) {
         /*
         استقبال عند الضغط علي زرار عرض الرقم
         اظهار الرقم الصحيح في alert dialog
          */
-        new AlertDialog.Builder(TestActivity.this)
-                .setMessage("الاجابة الصحيحة هي : " + photosModels.get(pos).getPhoto_number())
-                .setPositiveButton("تم", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
+
+
+        final EditText input = new EditText(TestActivity.this);
+        input.setHint("اكتب الرقم هنا");
+        input.setGravity(Gravity.CENTER);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        input.setLayoutParams(lp);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(TestActivity.this);
+        alertDialog.setMessage("تحقق من الرقم الموجود بالصورة");
+        alertDialog.setPositiveButton("تحقق", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String answer = input.getText().toString();
+                if (answer.isEmpty()) {
+                    Toast.makeText(TestActivity.this, "الرقم مطلوب", Toast.LENGTH_SHORT).show();
+                } else {
+                    dialog.dismiss();
+                    String result = "";
+                    String number = photosModels.get(pos).getPhoto_number();
+                    if (answer.equals(number)) {
+                        result = "إجابة صحيحة";
+                    } else {
+                        result = "خطأ \nالاجابة الصحيحة هي : " + number;
                     }
-                }).show();
+
+                    Toast.makeText(TestActivity.this, result, Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+        alertDialog.setNegativeButton("إالغاء", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+            }
+        });
+
+
+        alertDialog.setView(input);
+        alertDialog.show();
+        // }
+
     }
 }
